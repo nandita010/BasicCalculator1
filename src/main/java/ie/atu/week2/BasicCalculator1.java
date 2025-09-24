7628da32db29615e26933ad2b11ed463f829d537
Nandita Reji <g00463584@atu.ie>
2025-09-24 17:40:47 +0100

    some more

--- src/main/java/ie/atu/week2/BasicCalculator1.java
+++ src/main/java/ie/atu/week2/BasicCalculator1.java
@@ -8,7 +8,7 @@ public class BasicCalculator1 {
 
         //Prompt the user for a second number
         //Display the total to the screen
-        Scanner scan1= new Scanner(System.in);
+        Scanner scan1 = new Scanner(System.in);
 
         //Input first number
         System.out.println("Enter your first number: ");
@@ -22,5 +22,30 @@ public class BasicCalculator1 {
 
         double sum = firstNum + secondNum;
         System.out.println("The sum is " + sum);
+
+        System.out.println("Choose Operator: ");
+        String operation = scan1.next();
+
+        double result = switch (operation) {
+            case "add" -> {
+                yield firstNum + secondNum;
+            }
+            case "subtract" -> {
+                yield firstNum - secondNum;
+            }
+            case "multiply" -> {
+                yield firstNum * secondNum;
+            }
+            case "divide" -> {
+                yield firstNum / secondNum;
+            }
+
+            default -> {
+                System.out.println("Invalid Input");
+                yield 0;
+            }
+
+        };
+        System.out.println("Result is: " + result);
     }
 }
